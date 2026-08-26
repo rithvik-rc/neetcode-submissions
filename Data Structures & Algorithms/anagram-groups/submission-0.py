08ea0a786003d5d2383376c12a0941bfe24ff836
rithvikc <rithvikc@gmail.com>
2026-08-26 13:17:41 -0700

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for string in strs:
+            key = "".join(sorted(string))
+            anagrams[key].append(string)
+
+        return list(anagrams.values())
+
+        
